d26c28b59302f61089b8000b5a0b212c0fc5aeed
BennettxD2001 <rbennettito1@gmail.com>
2023-11-15 19:36:45 -0600

    cambios Bennett

--- Seguro.php
+++ Seguro.php
@@ -2,54 +2,50 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carga de Archivos Segura</title>
+    <title>Evaluación Docente</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        form {
+            width: 400px;
+            margin: auto;
+            border: 1px solid #ccc;
+            padding: 20px;
+            border-radius: 8px;
+        }
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 15px;
+            box-sizing: border-box;
+        }
+        button {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
+    <h1>Evaluación Docente</h1>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Directorio para almacenar archivos
-    $uploadDirectory = "uploads/";
-
-    // Verificar si el directorio de carga existe, si no, crearlo
-    if (!file_exists($uploadDirectory)) {
-        mkdir($uploadDirectory, 0777, true);
-    }
-
-    // Validar si se subió un archivo
-    if (isset($_FILES["archivo"])) {
-        $archivo = $_FILES["archivo"];
-
-        // Validar si no hubo errores durante la carga
-        if ($archivo["error"] == UPLOAD_ERR_OK) {
-            // Validar el tipo de archivo permitido (ejemplo: solo imágenes)
-            $allowedTypes = array("image/jpeg", "image/png", "image/gif");
-            if (in_array($archivo["type"], $allowedTypes)) {
-                // Generar un nombre único para el archivo
-                $nombreArchivo = uniqid() . "_" . basename($archivo["name"]);
-
-                // Mover el archivo al directorio de carga
-                $rutaArchivo = $uploadDirectory . $nombreArchivo;
-                move_uploaded_file($archivo["tmp_name"], $rutaArchivo);
-
-                echo "¡Archivo cargado con éxito!";
-            } else {
-                echo "Error: Solo se permiten archivos de imagen (jpeg, png, gif).";
-            }
-        } else {
-            echo "Error al cargar el archivo. Código de error: " . $archivo["error"];
-        }
-    }
-}
-?>
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
-    <label for="archivo">Seleccione un archivo:</label>
-    <input type="file" name="archivo" id="archivo" accept=".jpg, .jpeg, .png, .gif" required>
-    <br>
-    <input type="submit" value="Cargar Archivo">
-</form>
-
+    <!-- Formulario para subir archivos de evaluación -->
+    <form action="procesar_evaluacion.php" method="post" enctype="multipart/form-data">
+        <label for="evidence">Subir Evaluación (PDF):</label>
+        <input type="file" name="evidence" accept=".pdf" required>
+        <br>
+        <small>Tamaño máximo: 2MB</small>
+        <br>
+        <button type="submit">Subir Evaluación</button>
+    </form>
 </body>
 </html>
---
+++ procesar_evaluacion.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Directorio donde se almacenarán las evaluaciones
+    $target_dir = "uploads/";
+    
+    // Generar un nombre único para la evaluación
+    $target_file = $target_dir . uniqid() . basename($_FILES["evidence"]["name"]);
+    
+    // Obtener el tipo MIME del archivo
+    $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    
+    // Validar el tipo MIME (solo permitir archivos PDF)
+    if ($fileType != "pdf") {
+        echo "Solo se permiten archivos PDF.";
+    } else {
+        // Validar el tamaño del archivo (2 Megabytes máximo)
+        if ($_FILES["evidence"]["size"] > 2 * 1024 * 1024) {
+            echo "El archivo es demasiado grande. Tamaño máximo permitido: 2MB.";
+        } else {
+            // Mover el archivo al directorio de destino
+            if (move_uploaded_file($_FILES["evidence"]["tmp_name"], $target_file)) {
+                echo "La evaluación se ha subido correctamente.";
+            } else {
+                echo "Error al subir el archivo.";
+            }
+        }
+    }
+}
+?>
